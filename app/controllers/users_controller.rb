6ce71fe5a37807e772712aa19e6e5a3c62a64f8e
Mihalis Fthenos <mihalisfthenos@icloud.com>
2017-08-03 14:21:32 -0400

    change user in controller back to instance variable ( we DO need @user in the views! )

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
 
   def new
-    user = User.new
+    @user = User.new
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
+    @user = User.new(user_params)
+    if @user.save
       # create a new session for the user
       session[:user_id] = @user.id
       # redirect to storefront
