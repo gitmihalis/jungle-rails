5c535faea37411c8c037c63a95e4263ae26541bc
Mihalis Fthenos <mihalisfthenos@icloud.com>
2017-08-03 12:54:14 -0400

    add user has_secure_password and form creates new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,24 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
-    end
+  end
 
   def create
-    end
+    @user = User.new(user_params)
+    if user.save
+      # create a new session for the user
+      # redirect to storefront
+    else
+      redirect_to '/signup'
+    end 
+    
+  end
 
+  #===#===#===#===#===#===#===#===#===#===#===#===#===#===
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
 end
