00460124deaa459f37a171b77743fffe8932af13
Mihalis Fthenos <mihalisfthenos@icloud.com>
2017-08-03 13:31:57 -0400

    add login form, sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if user.save
+    if @user.save
       # create a new session for the user
       session[:user_id] = @user.id
       # redirect to storefront
