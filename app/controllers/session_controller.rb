8f76c9ec451255a9580eb537ce75696ddeec06e0
Mihalis Fthenos <mihalisfthenos@icloud.com>
2017-08-03 13:42:13 -0400

    add login/logout logic to session controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,9 +4,22 @@ class SessionController < ApplicationController
   end
 
   def create
+    @user = User.find_by_email(params[:email])
+    # If the user exists AND the password is correct
+    if @user && @user.authenticate(params[:password])
+      # Save the user id inside the browser cookie. This is how we keep the user 
+      # logged in when they navigate around our website.
+      session[:user_id] = @user.user_id
+      redirect_to '/'
+    else
+      # If user's login doesn't work, send them back to the login form.
+      redirect_to '/login'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
   end
   
 end
