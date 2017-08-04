2ecfb3f1ae3a0e64fad97b98590d91e95dc1488d
Mihalis Fthenos <mihalisfthenos@icloud.com>
2017-08-04 15:30:36 -0400

    add Review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,10 @@
 class Review < ActiveRecord::Base
+  belongs_to :product
+  belongs_to :user
+  
+  validates :description, presence: true,
+                          length: { minimum: 8 }
+  validates :rating, numericality: { less_than: 6, greater_than_or_equal_to: 0 }
+
+
 end
