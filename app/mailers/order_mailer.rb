549fb2dc86ef5ead3770d7a21474b241562cc6e3
Mihalis Fthenos <mihalisfthenos@icloud.com>
2017-08-04 12:21:22 -0400

    create receipt mail text

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,7 +2,8 @@ class OrderMailer < ApplicationMailer
 
   def receipt(order)
     @order = order
+    @url = 'http://www.example.org'
     mail(to: order.email, subject: "Your receipt for Order##{order.id}")
   end
-  
+
 end
