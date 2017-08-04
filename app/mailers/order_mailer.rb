e84b921b2e80f7be63b49a1f8182cad1af00a12b
Mihalis Fthenos <mihalisfthenos@icloud.com>
2017-08-04 12:29:16 -0400

    fix mispelled vars in orders_controller + order_mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -3,7 +3,7 @@ class OrderMailer < ApplicationMailer
   def receipt(order)
     @order = order
     @url = 'http://www.example.org'
-    mail(to: order.email, subject: "Your receipt for Order##{order.id}")
+    mail(to: @order.email, subject: "Your receipt for Order##{@order.id}")
   end
 
 end
