da4f7a758f936b246545e7308e313f7603fe922a
Mihalis Fthenos <mihalisfthenos@icloud.com>
2017-08-08 14:54:34 -0400

    test user can click product to get details

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-
+RSpec.feature "ProductDetails", type: :feature, js: true do
   # SETUP
   before :each do
     @category = Category.create! name: "Apparel"
@@ -17,12 +16,11 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-  scenario "They see all products" do
-    # ACT
+  scenario 'view product details' do
+
     visit root_path
-
-    # DEBUG / VERIFY
-    expect(page).to have_css 'article.product', count: 10
+    page.first(".product").find('h4').click
+    expect(page).to have_css('article.product-detail', count: 1)
+    save_screenshot('product_details.png')
   end
-
 end
